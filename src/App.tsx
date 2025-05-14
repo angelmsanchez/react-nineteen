e80b056410da669d20b83e01ef395adf828a0939
Angel Muñoz <tok.amunoz@iberia.es>
2025-05-14 16:21:19 +0200

    add routing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,22 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
-import { useCountStore } from './zustand/counter';
-import { Counter } from './components/counter/Counter';
+import { Router } from './components/router/Router';
+import { QueryProvider } from './contexts';
+import { useEffect } from 'react';
+import { InterceptorService } from './shared/services';
 
 export default function App() {
-  const [countLocal, setCount] = useState(0);
-  const { increase, reset } = useCountStore();
+  useEffect(() => {
+    InterceptorService();
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {countLocal}
-        </button>
-        <button onClick={increase}>increase</button>
-        <button onClick={reset}>reset</button>
-        <Counter />
-      </div>
+      <BrowserRouter>
+        <QueryProvider>
+          <Router />
+        </QueryProvider>
+      </BrowserRouter>
     </>
   );
 }
